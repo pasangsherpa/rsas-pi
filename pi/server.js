79004e90407f70bf53e1b72cefb4c597dddb4c88
Pasang Sherpa <pasang_sh@hotmail.com>
2014-02-11 21:16:07 -0800

    able to read analog value

--- pi/server.js
+++ pi/server.js
@@ -11,14 +11,18 @@ var http = require('http'),
     CHANNEL = 0,
     value;
 
-// wpi.mcp3004Setup(BASE, CHANNEL);
+wpi.mcp3004Setup(BASE, CHANNEL);
 wpi.setup('sys');
 
 http.createServer(function(req,resp) {
     resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World");
-    resp.end();
-    
-    console.log("sample output to console");
+    resp.write("Check the console bro..");
+	resp.end();
+}).listen(8000);
 
-}).listen(8080);
+setInterval(function() {
+   	value = wpi.analogRead(BASE);
+    console.log("value: "+ value);		
+}, 500);
+    
+
