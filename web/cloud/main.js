303bbce7b2ca7cf73291efb79504b1f699d09d26
pasang sherpa <pasang_sh@hotmail.com>
2014-03-10 20:17:43 -0700

    disable popup

--- web/cloud/main.js
+++ web/cloud/main.js
@@ -1,12 +1,14 @@
 require('cloud/app');
 
 Parse.Cloud.afterSave("Activity", function(request, response) {
-	var query = new Parse.Query(Parse.Installation);
+    var query = new Parse.Query(Parse.Installation);
     Parse.Push.send({
         where: query,
         data: {
             alert: "Someone is in the room. Wanna see the face?",
             title: "Room Security Alert System"
+            // action: "com.rsaspi.UPDATE_STATUS",
+            // customdata: "Someone is in the room. Do you wanna see the face?"
         }
     }, {
         success: function() {
